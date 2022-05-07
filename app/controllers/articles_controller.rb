5b521d814529028fba7784f5eb98505d69991059
vasily.smolin <vasilysmoolin@gmail.com>
2022-05-07 12:14:22 +0400

    finish

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+
+
+  http_basic_authenticate_with name: "rb", password: "secret", except: [:index, :show]
+
   def index
   @articles = Article.all
   end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
-  belongs_to :article
+      include Visible
+      belongs_to :article
 end
